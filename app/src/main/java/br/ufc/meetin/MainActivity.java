31e66e4e62f162ad42fd5a00adb17d3970fa3155
Diego <diego929@gmail.com>
2019-10-13 22:30:26 -0300

    Acesso da tela de login pela tela principal adicionado.

--- app/src/main/java/br/ufc/meetin/MainActivity.java
+++ app/src/main/java/br/ufc/meetin/MainActivity.java
@@ -2,13 +2,34 @@ package br.ufc.meetin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
+
 import android.os.Bundle;
 
+import br.ufc.meetin.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton initButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initButton = findViewById(R.id.startingButton);
+        initButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
